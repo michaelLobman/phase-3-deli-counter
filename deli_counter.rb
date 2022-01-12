f7a64a537c5e95fcb4f10fa14a3ac2a748fa7ad9
Michael Lobman <michael.lobman@gmail.com>
2022-01-11 21:41:18 -0500

    Complete assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def line array
+    if array.empty?
+        puts "The line is currently empty."
+    else
+        string_array = array.map.with_index(1) do |customer, index|
+             " #{index}. #{customer}"
+        end
+        puts "The line is currently:#{string_array.join}"
+    end
+end
+
+def take_a_number array, name
+    array << name
+    position = array.index(name) + 1
+    puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+def now_serving array
+    if array.empty?
+       puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array.shift}."
+    end
+end
+
